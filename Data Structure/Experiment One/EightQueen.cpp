08854b59bc8ebfd080d546eb1f45e23872827062
Yinhao Wang <njwangyh@gmail.com>
2018-04-10 09:29:06 +0800

    complete eight queen

--- Data Structure/Experiment One/EightQueen.cpp
+++ Data Structure/Experiment One/EightQueen.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 using namespace std;
-bool check(int chess[8][8],int row,int col) {
+bool check(int chess[8][8], int row, int col) {
 	//check vertical and horiontal
 	for (int i = 0;i < 8;++i) {
 		if (chess[row][i] == 1)
@@ -11,32 +11,21 @@ bool check(int chess[8][8],int row,int col) {
 			return false;
 	}
 	//check diagonal
-	int tmp_row=0, tmp_col=0;
-	tmp_row = (row > col) ? 8 : row + (8 - col);
-	tmp_col = col + (tmp_row - row);
+	int tmp_row = row, tmp_col = col;
 	while (tmp_row >= 0 && tmp_col >= 0) {
-		if (chess[tmp_row][tmp_col]==1)
-		{
-			return false;
-		}
-		tmp_row--;
-		tmp_col--;
+		if (chess[tmp_row--][tmp_col--] == 1)return false;
 	}
-
-	tmp_row = row;
-	tmp_col = col;
-	while (tmp_row <7 && tmp_col>1)
-	{
-		tmp_row++;
-		tmp_col--;
+	tmp_row = row, tmp_col = col;
+	while (tmp_row<8 && tmp_col<8) {
+		if (chess[tmp_row++][tmp_col++] == 1)return false;
 	}
-	while (tmp_row>=0 && tmp_col<8)
-	{
-		if (chess[tmp_row][tmp_col] == 1) {
-			return false;
-		}
-		tmp_row--;
-		tmp_col++;
+	tmp_row = row, tmp_col = col;
+	while (tmp_row >= 0 && tmp_col<8) {
+		if (chess[tmp_row--][tmp_col++] == 1)return false;
+	}
+	tmp_row = row, tmp_col = col;
+	while (tmp_row<8 && tmp_col >= 0) {
+		if (chess[tmp_row++][tmp_col--] == 1)return false;
 	}
 	return true;
 }
@@ -50,26 +39,28 @@ void print(int chess[8][8]) {
 	}
 	cout << endl;
 }
-void EQueen(int chess[8][8]) {
-	//for (int k = 0;k < 8;++k) {
-		for (int i = 0;i < 8;i++)
-		{
-			for (int j = 0;j < 8;j++) {
-				if (check(chess, i, j)) {
-					chess[i][j] = 1;
-				}
+void EQueen(int chess[8][8], int row) {
+	if (8 == row) {
+		static int total = 0;
+		total++;
+		cout << total << endl;
+		print(chess);
+	}
+	else {
+		for (int i=0;i <8; i++) {
+			for(int j = 0;j < 8;j++) chess[row][j] = 0;//回溯时清空标记非常重要
+			if (check(chess, row, i)) {
+				chess[row][i] = 1;
+				print(chess);
+				EQueen(chess, row + 1);
 			}
 		}
-		print(chess);
-	//}
+	}
 }
 int main(int argc, char const *argv[]) {
-	int chessboard[8][8];
-	for (int i = 0;i < 8;++i)
-		for (int j = 0;j < 8;++j)
-			chessboard[i][j] = 0;
+	int chessboard[8][8] = { 0 };
 	print(chessboard);
-	EQueen(chessboard);
+	EQueen(chessboard, 0);
 
 	system("pause");
 	return 0;

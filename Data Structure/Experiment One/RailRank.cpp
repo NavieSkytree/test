695c79c6efd3adc7e379f4d544e063fc117d0205
Yinhao Wang <njwangyh@gmail.com>
2018-04-15 19:19:43 +0800

    complete railrank

--- Data Structure/Experiment One/RailRank.cpp
+++ Data Structure/Experiment One/RailRank.cpp
@@ -3,23 +3,43 @@
 #include<deque>
 #include<vector>
 using namespace std;
-int train[10]={2,0,1,5,8,9,7,3,4,6};;
+int train[10] = { 2,0,1,5,8,9,7,3,4,6 };
 vector< queue<int> > bufferarray;
-// void check_input(int i){
-//   for(auto i:bufferarray){
-//
-//   }
-//   if(bufferarray.empty())
-//     // bufferarray.append(queue<int>(i));
-// }
+void check_input(int i) {
+	for (auto it=bufferarray.begin();it<bufferarray.end();it++) {
+		if (i>it->back()) {
+			it->push(i);
+			return;
+		}
+	}
+	queue<int> tmp;
+	tmp.push(i);
+	bufferarray.push_back(tmp);
+}
+void print(){
+  int tmp=-1,minPos=-1;
+  for (int i = 0; i < 10; i++) {
+    minPos=0;
+    tmp=1000;//一个极大值保证正常运行
+    for(int j=0;j<bufferarray.size();++j){
+      if(bufferarray[j].empty())
+        continue;
+      // tmp=bufferarray[j].front();
+      if(bufferarray[j].front()<tmp){
+        tmp=bufferarray[j].front();
+        minPos=j;
+      }
+    }
+    cout << tmp<<" ";
+    bufferarray[minPos].pop();
+  }
+}
 int main(int argc, char const *argv[]) {
-  for(auto i:train){
-
-  }
-  deque<int> mydeck (3,100);
-  queue<int> q(1,10);
-  for (auto i:mydeck) {
-    cout<<i;
-  }
-  return 0;
+	for (auto i : train) {
+		check_input(i);
+	}
+  // for(auto k:bufferarray)cout<<k.size();
+	print();
+	system("pause");
+	return 0;
 }
